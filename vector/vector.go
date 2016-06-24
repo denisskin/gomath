3d985721a51b0b85b59aef25c1044b34a3bcdd53
denis <d.glazkov@corp.badoo.com>
2016-06-24 15:03:30 +0300

    fix String function

--- vector/vector.go
+++ vector/vector.go
@@ -4,7 +4,7 @@ import "fmt"
 
 type Vector []float64
 
-// NewVector creates a Vector with default value
+// NewVector creates a Vector with default value.
 func NewVector(size int, defVal float64) Vector {
 	v := make(Vector, size)
 	if defVal != 0 {
@@ -15,7 +15,7 @@ func NewVector(size int, defVal float64) Vector {
 	return v
 }
 
-// Range creates a Vector containing a range of elements
+// Range creates a Vector containing a range of elements.
 func Range(start, end float64, size int) Vector {
 	v := make(Vector, size)
 	n := size - 1
@@ -26,46 +26,46 @@ func Range(start, end float64, size int) Vector {
 	return v
 }
 
-// Size returns the length of the vector
+// Size returns the length of the vector.
 func (v Vector) Size() int {
 	return len(v)
 }
 
-// String returns vector as string
+// String returns vector as string.
 func (v Vector) String() string {
-	return fmt.Sprintf("[%v]", v)
+	return fmt.Sprintf("%v", []float64(v))
 }
 
-// Clone creates a copy of current vector
+// Clone creates a copy of current vector.
 func (v Vector) Clone() Vector {
 	v1 := make(Vector, len(v))
 	copy(v1, v)
 	return v1
 }
 
-// Push adds an element to the end of the vector
+// Push adds an element to the end of the vector.
 func (v Vector) Push(x float64) {
 	v = append(v, x)
 }
 
-// Pop removes an element from the end of the vector
+// Pop removes an element from the end of the vector.
 func (v Vector) Pop() (x float64) {
 	x, v = v[len(v)-1], v[:len(v)-1]
 	return
 }
 
-// Shift adds an element to the beginning of the vector
+// Shift adds an element to the beginning of the vector.
 func (v Vector) Shift() (x float64) {
 	x, v = v[0], v[1:]
 	return
 }
 
-// Unshift removes an element from the beginning of the vector
+// Unshift removes an element from the beginning of the vector.
 func (v Vector) Unshift(x float64) {
 	v = append(Vector{x}, v...)
 }
 
-// Map applies the callback function to the elements of the vector and returns new Vector with results
+// Map applies the callback function to the elements of the vector and returns new Vector with results.
 func (v Vector) Map(fn func(float64) float64) Vector {
 	v1 := make(Vector, len(v))
 	for i, x := range v {
