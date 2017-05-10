330a79deb863ce86c00012426edaf1797b9bd3c3
denisskin <denis@begom.com>
2017-05-10 12:45:07 +0300

    change tests

--- regression/regression_test.go
+++ regression/regression_test.go
@@ -1,11 +1,12 @@
 package regression
 
 import (
-	"github.com/denisskin/gomath/vector"
-	"github.com/stretchr/testify/assert"
 	"math"
 	"math/rand"
 	"testing"
+
+	"github.com/denisskin/gomath/vector"
+	"github.com/stretchr/testify/assert"
 )
 
 func fnLine(x float64) float64 {
@@ -17,39 +18,48 @@ func fnPolynom2(x float64) float64 {
 func fnPolynom3(x float64) float64 {
 	return x*x*x - 2*x*x + 3*x - 4
 }
-func fnRunge(x float64) float64 {
+func fnRungeFunc(x float64) float64 {
 	return 1 / (1 + x*x)
 }
 
-// testRegression tests regression function and returns deviation
-func testRegression(regression RegressionFunc, fn Func) (dev float64) {
-
-	X := vector.Range(0, 10, 100)
-
-	Y := X.Map(func(x float64) float64 {
-		// add random perturbation (±3%)
-		return fn(x) * (1 + 2*(rand.Float64()-0.5)*0.03)
-	})
-
-	fnReg := regression(X, Y)
-
-	x0 := X.Avg()
-
-	return math.Abs(fnReg(x0)/fn(x0) - 1)
-}
-
 func TestLinearRegression(t *testing.T) {
-	assert.True(t, testRegression(LinearRegression, fnLine) < 0.01)
-	assert.True(t, testRegression(LinearRegression, fnPolynom2) > 0.40)
-	assert.True(t, testRegression(LinearRegression, fnPolynom3) > 1.20)
-	assert.True(t, testRegression(LinearRegression, fnRunge) > 2.90)
+	x0 := 6.666
+	X := vector.Range(0, 10, 100)
+	regressionFunc := LinearRegression
+
+	// tests the regression function and returns deviation for x0
+	regressionError := func(fn Func) (dev float64) {
+		Y := X.Map(func(x float64) float64 {
+			// add random perturbation (±3%)
+			return fn(x) * (1 + 2*(rand.Float64()-0.5)*0.03)
+		})
+		fnReg := regressionFunc(X, Y)
+		return math.Abs(fnReg(x0)/fn(x0) - 1)
+	}
+
+	assert.InDelta(t, 0.00, regressionError(fnLine), 0.01)
+	assert.InDelta(t, 0.16, regressionError(fnPolynom2), 0.01)
+	assert.InDelta(t, 0.42, regressionError(fnPolynom3), 0.01)
+	assert.InDelta(t, 1.18, regressionError(fnRungeFunc), 0.01)
 }
 
 func TestNewInterpolationRegression(t *testing.T) {
-	interpolationRegression := NewInterpolationRegression(0, 0)
+	x0 := 6.666
+	X := vector.Range(0, 10, 100)
+	regressionFunc := NewInterpolationRegression(0, 0)
 
-	assert.True(t, testRegression(interpolationRegression, fnLine) < 0.01)
-	assert.True(t, testRegression(interpolationRegression, fnPolynom2) < 0.01)
-	assert.True(t, testRegression(interpolationRegression, fnPolynom3) < 0.01)
-	assert.True(t, testRegression(interpolationRegression, fnRunge) < 0.01)
+	// tests the regression function and returns deviation for x0
+	regressionError := func(fn Func) (dev float64) {
+		Y := X.Map(func(x float64) float64 {
+			// add random perturbation (±3%)
+			return fn(x) * (1 + 2*(rand.Float64()-0.5)*0.03)
+		})
+		fnReg := regressionFunc(X, Y)
+		return math.Abs(fnReg(x0)/fn(x0) - 1)
+	}
+
+	assert.InDelta(t, 0.00, regressionError(fnLine), 0.01)
+	assert.InDelta(t, 0.00, regressionError(fnPolynom2), 0.01)
+	assert.InDelta(t, 0.00, regressionError(fnPolynom3), 0.01)
+	assert.InDelta(t, 0.00, regressionError(fnRungeFunc), 0.01)
 }
